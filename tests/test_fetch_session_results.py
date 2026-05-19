604e8e976506908fe00e9560c9196a321de60424
Maksymilian Stolarek <maksiu2003xd@gmail.com>
2026-05-19 21:20:10 +0200

    test(services): add happy path test for fetch_session_results

---
+++ tests/test_fetch_session_results.py
@@ -0,0 +1,26 @@
+from unittest.mock import patch
+import pytest
+
+
+
+@pytest.mark.asyncio
+@patch('services._fetch_openf1')
+async def test_fetch_session_results_returns_dict_of_results(mock_fetch_openf1):
+
+    fake_session_results = [
+        {"position": 1, "driver_number": 44, "number_of_laps": 44, "points": 25,
+            "dnf": False, "dns": False, "dsq": False,
+            "duration": 4797.566, "gap_to_leader": 0, "meeting_key": 1242, "session_key": 9574},
+        ]
+
+    mock_fetch_openf1.return_value = fake_session_results
+    session_key = 9574
+    from services import fetch_session_results
+    result = await fetch_session_results(session_key=session_key)
+
+    assert str(session_key) in mock_fetch_openf1.call_args.args[0] 
+    assert result == fake_session_results
+
+
+
+
