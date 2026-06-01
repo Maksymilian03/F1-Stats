5fa939860aab590e1a4efbaa7fdcd9839cbf3f25
Maksymilian Stolarek <maksiu2003xd@gmail.com>
2026-06-01 15:37:33 +0200

    test: add get_constructor_standings tests, expand fake_drivers fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,10 @@ def fake_drivers():
     return {
         44: {"driver_number": 44, "full_name": "Lewis Hamilton", "team_name": "Mercedes"},
         3: {"driver_number": 3, "full_name": "Max Verstappen", "team_name": "Red Bull"},
-        16: {"driver_number": 16, "full_name": "Charles Leclerc", "team_name": "Ferrari"}
+        16: {"driver_number": 16, "full_name": "Charles Leclerc", "team_name": "Ferrari"},
+        6: {"driver_number": 6, "full_name": "Sergio Perez", "team_name": "Red Bull"},
+        45: {"driver_number": 45, "full_name": "Carlos Sainz", "team_name": "Ferrari"},
+        77: {"driver_number": 77, "full_name": "Valtteri Bottas", "team_name": "Mercedes"},   
     }
 
 @pytest.fixture
@@ -13,13 +16,24 @@ def fake_sprints_results():
     return [
   [{"position": 1, "driver_number": 3, "points": 8, "session_key": 9579},
     {"position": 2, "driver_number": 44, "points": 7, "session_key": 9579},
-    {"position": 3, "driver_number": 16, "points": 6, "session_key": 9579}],
+    {"position": 3, "driver_number": 16, "points": 6, "session_key": 9579},
+    {"position": 4, "driver_number": 6, "points": 5, "session_key": 9579},
+    {"position": 5, "driver_number": 45, "points": 4, "session_key": 9579},
+    {"position": 6, "driver_number": 77, "points": 3, "session_key": 9579}],
+
   [{"position": 1, "driver_number": 44, "points": 8, "session_key": 9888},
     {"position": 2, "driver_number": 3, "points": 7, "session_key": 9888},
-    {"position": 3, "driver_number": 16, "points": 6, "session_key": 9888}],
+    {"position": 3, "driver_number": 16, "points": 6, "session_key": 9888},
+    {"position": 4, "driver_number": 6, "points": 5, "session_key": 9888},
+    {"position": 5, "driver_number": 45, "points": 4, "session_key": 9888},
+    {"position": 6, "driver_number": 77, "points": 3, "session_key": 9888}],
+
   [{"position": 1, "driver_number": 16, "points": 8, "session_key": 9571},
     {"position": 2, "driver_number": 3, "points": 7, "session_key": 9571},
-    {"position": 3, "driver_number": 44, "points": 6, "session_key": 9571}]
+    {"position": 3, "driver_number": 44, "points": 6, "session_key": 9571},
+    {"position": 4, "driver_number": 6, "points": 5, "session_key": 9571},
+    {"position": 5, "driver_number": 45, "points": 4, "session_key": 9571},
+    {"position": 6, "driver_number": 77, "points": 3, "session_key": 9571}]
   ]
 
 @pytest.fixture
@@ -27,13 +41,24 @@ def fake_races_results():
     return [
     [{"position": 1, "driver_number": 3, "points": 25, "session_key": 9575},
         {"position": 2, "driver_number": 44, "points": 18, "session_key": 9575},
-        {"position": 3, "driver_number": 16, "points": 15, "session_key": 9575}],
+        {"position": 3, "driver_number": 16, "points": 15, "session_key": 9575},
+        {"position": 4, "driver_number": 6, "points": 12, "session_key": 9575},
+        {"position": 5, "driver_number": 45, "points": 10, "session_key": 9575},
+        {"position": 6, "driver_number": 77, "points": 8, "session_key": 9575}],
+
     [{"position": 1, "driver_number": 44, "points": 25, "session_key": 9573},
         {"position": 2, "driver_number": 3, "points": 18, "session_key": 9573},
-        {"position": 3, "driver_number": 16, "points": 15, "session_key": 9573}],
+        {"position": 3, "driver_number": 16, "points": 15, "session_key": 9573},
+        {"position": 4, "driver_number": 6, "points": 12, "session_key": 9573},
+        {"position": 5, "driver_number": 45, "points": 10, "session_key": 9573},
+        {"position": 6, "driver_number": 77, "points": 8, "session_key": 9573}],
+
     [{"position": 1, "driver_number": 3, "points": 25, "session_key": 9572},
         {"position": 2, "driver_number": 16, "points": 18, "session_key": 9572},
-        {"position": 3, "driver_number": 44, "points": 15, "session_key": 9572}]    
+        {"position": 3, "driver_number": 44, "points": 15, "session_key": 9572},
+        {"position": 4, "driver_number": 6, "points": 12, "session_key": 9572},
+        {"position": 5, "driver_number": 45, "points": 10, "session_key": 9572},
+        {"position": 6, "driver_number": 77, "points": 8, "session_key": 9572}]
     ]
 
 @pytest.fixture
---
+++ tests/test_get_constructor_standings.py
@@ -0,0 +1,139 @@
+from unittest.mock import patch
+import pytest, json
+from fastapi.testclient import TestClient
+from main import app
+
+
+from services import get_constructor_standings, CURRENT_YEAR
+
+
+@pytest.mark.asyncio
+@patch('services.get_races_and_sprints')
+@patch('services.fetch_session_with_semaphore')
+@patch('services.fetch_drivers')
+async def test_get_constructor_standings_returns_correct_position(drivers_mock, session_mock, races_and_sprints_mock,
+     tmp_path, monkeypatch, fake_drivers, fake_sprints_results, fake_races_results, fake_race_keys, fake_sprint_keys):
+    
+    # Arrange
+    monkeypatch.setattr('services.CACHE_DIR', str(tmp_path))
+
+    drivers_mock.return_value = fake_drivers
+    races_and_sprints_mock.return_value = (fake_race_keys, fake_sprint_keys)
+    session_mock.side_effect = fake_races_results + fake_sprints_results
+
+    # Act
+    result = await get_constructor_standings(2023)
+
+    # Assert
+    assert result[0]['team'] == "Red Bull"
+    assert result[1]['team'] == "Mercedes"
+    assert result[2]['team'] == "Ferrari"
+
+@pytest.mark.asyncio
+@patch('services.get_races_and_sprints')
+@patch('services.fetch_session_with_semaphore')
+@patch('services.fetch_drivers')
+async def test_get_constructor_standings_calculates_total_points_correctly(drivers_mock, session_mock, races_and_sprints_mock,
+         tmp_path, monkeypatch, fake_drivers, fake_sprints_results, fake_races_results, fake_race_keys, fake_sprint_keys):
+    
+    # Arrange
+    monkeypatch.setattr('services.CACHE_DIR', str(tmp_path))
+
+    drivers_mock.return_value = fake_drivers
+    races_and_sprints_mock.return_value = (fake_race_keys, fake_sprint_keys)
+    session_mock.side_effect = fake_races_results + fake_sprints_results
+
+    # Act
+    result = await get_constructor_standings(2023)
+
+    # Assert
+    max_verstappen_points = (25 + 18 + 25 + 8 + 7 + 7)
+    sergio_perez_points = (12 + 12 + 12 + 5 + 5 + 5)
+    red_bull_points = max_verstappen_points + sergio_perez_points
+
+    lewis_hamilton_points = (18 + 25 + 15 + 7 + 8 + 6)
+    valtteri_bottas_points = (8 + 8 + 8 + 3 + 3 + 3)
+    mercedes_points = lewis_hamilton_points + valtteri_bottas_points
+
+    charles_leclerc_points = (15 + 15 + 18 + 6 + 6 + 8)
+    carlos_sainz_points = (10 + 10 + 10 + 4 + 4 + 4)
+    ferrari_points = charles_leclerc_points + carlos_sainz_points
+
+    assert result[0]['points'] == red_bull_points
+    assert result[1]['points'] == mercedes_points
+    assert result[2]['points'] == ferrari_points
+
+@pytest.mark.asyncio    
+@patch('services.get_races_and_sprints')
+@patch('services.fetch_session_with_semaphore')
+@patch('services.fetch_drivers')
+async def test_get_constructor_standings_does_not_count_sprint_wins_as_total_wins(drivers_mock, session_mock, races_and_sprints_mock,
+         tmp_path, monkeypatch, fake_drivers, fake_sprints_results, fake_races_results, fake_race_keys, fake_sprint_keys):
+    
+    # Arrange
+    monkeypatch.setattr('services.CACHE_DIR', str(tmp_path))
+
+    drivers_mock.return_value = fake_drivers
+    races_and_sprints_mock.return_value = (fake_race_keys, fake_sprint_keys)
+    session_mock.side_effect = fake_races_results + fake_sprints_results
+
+    # Act
+    result = await get_constructor_standings(2023)
+
+    # Assert
+
+    max_verstappen_wins = 2
+    sergio_perez_wins = 0
+    red_bull_wins = max_verstappen_wins + sergio_perez_wins
+
+    lewis_hamilton_wins = 1
+    valtteri_bottas_wins = 0
+    mercedes_wins = lewis_hamilton_wins + valtteri_bottas_wins
+
+    charles_leclerc_wins = 0
+    carlos_sainz_wins = 0 
+    ferrari_wins = charles_leclerc_wins + carlos_sainz_wins
+
+    assert result[0]['wins'] == red_bull_wins
+    assert result[1]['wins'] == mercedes_wins
+    assert result[2]['wins'] == ferrari_wins
+
+
+@pytest.mark.asyncio
+@patch('services.get_races_and_sprints')     
+async def test_get_constructor_standings_returns_empty_list_when_no_races_or_sprints(races_and_sprints_mock,
+                                                                                      tmp_path, monkeypatch):
+    # Arrange
+    monkeypatch.setattr('services.CACHE_DIR', str(tmp_path))
+
+    races_and_sprints_mock.return_value = ([], [])
+
+    # Act
+    result = await get_constructor_standings(2026)
+
+    # Assert
+    assert result == []
+
+
+client = TestClient(app)
+def test_get_constructor_standings_raise_422_when_year_is_less_than_2023():
+    # Arrange
+    year = 2000 
+
+    # Act
+    response = client.get(f"/standings/constructors/{year}/")
+
+    # Assert
+    assert response.status_code == 422
+
+def test_get_constructor_standings_raise_422_when_year_is_greater_than_current_year():
+    # Arrange
+    year = CURRENT_YEAR + 1
+
+    # Act
+    response = client.get(f"/standings/constructors/{year}/")
+
+    # Assert
+    assert response.status_code == 422
+
+
