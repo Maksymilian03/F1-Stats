16dd84905a02d44416100e631cb33faf8b06d42f
Maksymilian03 <maksiu2003xd@gmail.com>
2026-06-30 20:14:30 +0200

    feat(compare): add /compare/{year}/{driver1}/{driver2}/ endpoint

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from fastapi import Depends, FastAPI, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import Base, engine, get_db
-from schemas import ConstructorEntry, Driver, RaceResult, StandingsEntry
+from schemas import CompareResponse, ConstructorEntry, Driver, RaceResult, StandingsEntry
 from services import (
     CURRENT_YEAR,
     fetch_drivers,
+    get_comparison_drivers,
     get_constructor_standings,
     get_driver_standings,
     get_race_results,
@@ -61,3 +62,26 @@ async def standings_endpoint(
     session: AsyncSession = Depends(get_db) # noqa B008
     ):
     return await get_driver_standings(year, session)
+
+
+@app.get('/compare/{year}/{driver1_number}/{driver2_number}/', response_model=CompareResponse)
+async def compare_endpoint(
+    year: int = Path(
+        ..., ge=2023, le=CURRENT_YEAR,
+        description=f"Rok musi być pomiędzy 2023 a {CURRENT_YEAR}"
+        ),
+    driver1_number: int = Path(
+        ...,
+        ge=1,
+        le=99,
+        description="Number of first driver must be 1-99"
+    ),
+    driver2_number: int = Path(
+        ...,
+        ge=1,
+        le=99,
+        description="Number of second driver must be 1-99"
+    ),
+    session: AsyncSession = Depends(get_db) # noqa B008
+):
+    return await get_comparison_drivers(year, driver1_number, driver2_number, session)
